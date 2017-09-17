9d32b67c63d868e4c608968b826f16f6cb18a7b8
alexwalz <alexwalz@icloud.com>
2017-09-17 15:25:44 -0600

    New repo

---
+++ PREWORK_AW/MODULE-10/javascript.js
@@ -0,0 +1,15 @@
+function changeColor(box) {
+    document.getElementById(box).style.backgroundColor = "blue";
+}
+
+function changeOpacity(box) {
+    document.getElementById(box).style.opacity = "0.5";
+}
+
+function changeSize(box) {
+    document.getElementById(box).style.width = "400px";
+}
+
+function reset(box) {
+    document.getElementById(box).style.width = "width:150px".style.background - color = "orange";
+}
