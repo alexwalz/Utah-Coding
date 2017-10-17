0e5fe5391239ee5fa098e0d56f836b7ccc7c3477
alexwalz <alexwalz@icloud.com>
2017-10-16 18:50:10 -0600

    week 6

--- week05/Activities/10-Stopwatch/Unsolved/stopwatch.js
+++ week05/Activities/10-Stopwatch/Unsolved/stopwatch.js
@@ -3,11 +3,11 @@
 //  This code will run as soon as the page loads.
 window.onload = function() {
 
-  //  Click events are done for us:
-  $("#lap").click(stopwatch.recordLap);
-  $("#stop").click(stopwatch.stop);
-  $("#reset").click(stopwatch.reset);
-  $("#start").click(stopwatch.start);
+    //  Click events are done for us:
+    $("#lap").click(stopwatch.recordLap);
+    $("#stop").click(stopwatch.stop);
+    $("#reset").click(stopwatch.reset);
+    $("#start").click(stopwatch.start);
 };
 
 //  Variable that will hold our setInterval that runs the stopwatch
@@ -19,73 +19,69 @@ var clockRunning = false;
 //  Our stopwatch object.
 var stopwatch = {
 
-  time: 0,
-  lap: 1,
+    time: 0,
+    lap: 1,
 
-  reset: function() {
+    reset: function() {
 
-    stopwatch.time = 0;
-    stopwatch.lap = 1;
+        stopwatch.time = 0;
+        stopwatch.lap = 1;
 
-    //  TODO: Change the "display" div to "00:00."
+        //  TODO: Change the "display" div to "00:00."
 
-  },
+    },
 
-  start: function() {
+    start: function() {
 
-      //  TODO: Use setInterval to start the count here and set the clock to running.
-      if (!clockRunning) {
+        //  TODO: Use setInterval to start the count here and set the clock to running.
+        if (!clockRunning) {
+            intervalId = stopwatch.count;
+            clockRunning = true;
+        }
 
-      }
+    },
 
-  },
-  stop: function() {
 
-    //  TODO: Use clearInterval to stop the count here and set the clock to not be running.
+    stop: function() {
 
-  },
+        //  TODO: Use clearInterval to stop the count here and set the clock to not be running.
 
-  recordLap: function() {
+    },
 
-      //  TODO: Get the current time, pass that into the stopwatch.timeConverter function,
-      //        and save the result in a variable.
+    recordLap: function() {
 
-      //  TODO: Add the current lap and time to the "laps" div.
+        //  TODO: Get the current time, pass that into the stopwatch.timeConverter function,
+        //        and save the result in a variable.
 
-      //  TODO: Increment lap by 1. Remember, we can't use "this" here.
-  },
-  count: function() {
+        //  TODO: Add the current lap and time to the "laps" div.
 
-    //  TODO: increment time by 1, remember we cant use "this" here.
+        //  TODO: Increment lap by 1. Remember, we can't use "this" here.
+    },
+    count: function() {
 
-    //  TODO: Get the current time, pass that into the stopwatch.timeConverter function,
-    //        and save the result in a variable.
+        intervalId++
 
-    //  TODO: Use the variable you just created to show the converted time in the "display" div.
+    },
 
-  },
+    //  THIS FUNCTION IS DONE FOR US!
+    //  We do not need to touch it.
 
-  //  THIS FUNCTION IS DONE FOR US!
-  //  We do not need to touch it.
+    timeConverter: function(t) {
 
-  timeConverter: function(t) {
+        //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+        var minutes = Math.floor(t / 60);
+        var seconds = t - (minutes * 60);
 
-    //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
-    var minutes = Math.floor(t / 60);
-    var seconds = t - (minutes * 60);
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
 
-    if (seconds < 10) {
-      seconds = "0" + seconds;
+        if (minutes === 0) {
+            minutes = "00";
+        } else if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+
+        return minutes + ":" + seconds;
     }
-
-    if (minutes === 0) {
-      minutes = "00";
-    }
-
-    else if (minutes < 10) {
-      minutes = "0" + minutes;
-    }
-
-    return minutes + ":" + seconds;
-  }
 };
