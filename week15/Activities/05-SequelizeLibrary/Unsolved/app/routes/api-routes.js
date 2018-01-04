03b02a19bf304b01c2183a1fa4c7e1ad64dc8bde
alexwalz <alexwalz@icloud.com>
2018-01-03 21:21:13 -0700

    Sequelize Library App

--- week15/Activities/05-SequelizeLibrary/Unsolved/app/routes/api-routes.js
+++ week15/Activities/05-SequelizeLibrary/Unsolved/app/routes/api-routes.js
@@ -2,53 +2,103 @@
 // api-routes.js - this file offers a set of routes for displaying and saving data to the db
 // *********************************************************************************
 
-// Dependencies
-// =============================================================
+
 var Book = require("../models/book.js");
 
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
-  // Add sequelize code to get all books and return them as JSON
-  app.get("/api/all", function(req, res) {
+
+
+  app.get("/api/all", function (req, res) {
+    Book.findAll({}).then(function (results) {
+      res.json(results);
+    });
+  });
+
+
+  app.get("/api/:book", function (req, res) {
+    var title = req.params.book
+    Book.findAll({
+      where: {
+        title: title
+      }
+    }).then(function (results) {
+      res.json(results);
+    });
 
   });
 
-  // Add sequelize code to get a specific book and return it as JSON
-  app.get("/api/:book", function(req, res) {
+  app.get("/api/genre/:genre", function (req, res) {
+    var genre = req.params.genre
+    Book.findAll({
+      where: {
+        genre: genre
+      }
+    }).then(function (results) {
+      res.json(results);
+    });
+  });
+
+  app.get("/api/author/:author", function (req, res) {
+    var author = req.params.author
+    Book.findAll({
+      where: {
+        author: author
+      }
+    }).then(function (results) {
+      res.json(results);
+    });
+  });
+
+  app.get("/api/books/long", function (req, res) {
+    Book.findAll({
+      where: {
+        pages: {
+          $gte: 300
+        }
+      }
+    }).then(function (results) {
+      res.json(results);
+    });
 
   });
 
-  // Add sequelize code to get all books of a specific genre and return them as JSON
-  app.get("/api/genre/:genre", function(req, res) {
+  app.get("/api/books/short", function (req, res) {
+
+    Book.findAll({
+      where: {
+        pages: {
+          $lte: 210
+        }
+      }
+    }).then(function (results) {
+      res.json(results);
+    });
 
   });
 
-  // Add sequelize code to get all books from a specific author and return them as JSON
-  app.get("/api/author/:author", function(req, res) {
+  app.post("/api/new", function (req, res) {
 
-  });
-
-  // Add sequelize code to get all "long" books (more than 150 pages) and return them as JSON
-  app.get("/api/books/long", function(req, res) {
-
-  });
-
-  // Add sequelize code to get all "short" books (150 pages or less) and return them as JSON
-  app.get("/api/books/short", function(req, res) {
-
-  });
-
-  // Add sequelize code to create a book
-  app.post("/api/new", function(req, res) {
+    Book.create({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      pages: req.body.pages
+    }).then;
 
   });
 
   // Add sequelize code to delete a book
-  app.post("/api/delete", function(req, res) {
+  app.post("/api/delete", function (req, res) {
 
+    Book.destroy({
+      where: {
+        id: req.body.id
+      }
+    })
   });
 
 };
