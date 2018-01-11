5e8eaa5cc5a33d39941e9ddd6932dc48ecfc6a30
alexwalz <alexwalz@icloud.com>
2018-01-10 19:58:54 -0700

    Week 16 Testing

--- week16/Activities/02-Check-For-Exceptions/Unsolved/assert.js
+++ week16/Activities/02-Check-For-Exceptions/Unsolved/assert.js
@@ -9,7 +9,9 @@ var assertThrows = function (func, x, y) {
 };
 
 var multiply = function (x, y) {
-  if (x.typeof === 'string' || y.typeof === 'string') {
+  if (typeof x !== 'number' || typeof y !== 'number') {
     throw new Error("Error: One of your entries was not a number");
   } else return x * y;
 };
+
+
--- week16/Activities/04-Disemvowel/disemvowel.js
+++ week16/Activities/04-Disemvowel/disemvowel.js
@@ -1,3 +1,5 @@
+var expect = require("chai").expect;
+
 var disemvowel = function(str) {
   if (typeof str === "number") {
     return String(str);
@@ -6,3 +8,23 @@ var disemvowel = function(str) {
 };
 
 module.exports = disemvowel;
+
+var string = "Chase is a jerk"
+var newstring = "Chs s  jrk"
+
+
+
+describe("disemvowel", function() {
+  it("should remove all vowels from a string", function() {
+    expect(disemvowel(string)).to.be.a("string");
+    expect(disemvowel(string)).to.equal(newstring);
+  });
+
+
+  // it("should throw when not passed numbers", function() {
+  //   expect(function() {
+  //     multiply(2, "4");
+  //   }).to.throw(Error);
+  // }); 
+
+});
